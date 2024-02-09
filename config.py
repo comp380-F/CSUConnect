4f2c5a22841ce27e2c6ed4ef205a7e6a7fe4917f
Nver Khachoyan <khachoyannver@gmail.com>
2024-02-08 19:45:53 -0800

    removed heroku setup files

--- config.py
+++ config.py
@@ -2,9 +2,6 @@ import os
 
 database_uri = os.getenv('DATABASE_URL')
 
-if uri and uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-
 class Config:
     SECRET_KEY = 'my_secret_key'
     SQLALCHEMY_DATABASE_URI = database_uri or 'sqlite:///app.db'
