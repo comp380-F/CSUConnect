9abe8f88473745ac781654e0c062f22cdf3f56fa
Nver Khachoyan <khachoyannver@gmail.com>
2024-02-09 01:13:08 -0800

    removed extra files

--- config 2.py
+++
@@ -1,8 +0,0 @@
-import os
-
-database_uri = os.getenv('DATABASE_URL')
-
-class Config:
-    SECRET_KEY = 'my_secret_key'
-    SQLALCHEMY_DATABASE_URI = database_uri or 'sqlite:///app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
