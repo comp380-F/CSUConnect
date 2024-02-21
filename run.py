9f7d5d7b571895e7734777b57d956a0585395fe3
Nver Khachoyan <khachoyannver@gmail.com>
2024-02-21 00:53:20 -0800

    Railwing initial setup, gunicorn version changed to work with railwind

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
